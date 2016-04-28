59e1c6b21195a7480b3c987e8c00c13968d61750
Steve Minor <stv.mnr@gmail.com>
2016-04-27 18:20:53 -0700

    fixed shell script to work on OSX. need to verify still works on ubuntu

--- _posts/new_post.sh
+++ _posts/new_post.sh
@@ -13,13 +13,14 @@ post_date=$(date +'%Y-%m-%d %T %z')
 category=$1
 title=$2
 #format the title of the post into underscores and lowercase for the filename
-formatted_title="$(echo $localVar | sed -r 's/([A-Z])/\L\1/g;s/ /_/g')"
+formatted_title="$(echo $title | sed 's/ /_/g' | tr [:upper:] [:lower:])"
+
 
 #if the category of post doesn't exist, create the folder
-if [ ! -d _posts/$category ]; then
-  mkdir  _posts/$category 
+if [ ! -d $category ]; then
+  mkdir  $category 
 fi
 
 
 #write the file with the proper template
-echo "---\ntitle: $title\ndate: $post_date\ncategory: $category\n---" >> _posts/$category/$formatted_title.md
+echo -e "---\nlayout: post\ntitle: $title\ndate: $post_date\ncategory: $category\n---" >> $category/$filename_date-$formatted_title.md
